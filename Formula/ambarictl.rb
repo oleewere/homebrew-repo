bcda522d347ad5caed6e80444220a4e60fc16e01
oleewere <oleewere@gmail.com>
2018-09-16 21:38:27 +0200

    Brew formula update for ambarictl version v0.2.0

--- Formula/ambarictl.rb
+++ Formula/ambarictl.rb
@@ -1,9 +1,9 @@
 class Ambarictl < Formula
   desc "Software to manage multiple Ambari servers."
   homepage "https://github.com/oleewere"
-  url "https://github.com/oleewere/ambarictl/releases/download/v0.1.0/ambarictl_0.1.0_macOS_64-bit.tar.gz"
-  version "0.1.0"
-  sha256 "b55f49fb604dc83a0baa782b9b2408c5ac7901019ab89762614e074a207bf422"
+  url "https://github.com/oleewere/ambarictl/releases/download/v0.2.0/ambarictl_0.2.0_macOS_64-bit.tar.gz"
+  version "0.2.0"
+  sha256 "2bc721c1c44fbef3e803f1b39d17aa1459b0449ad80b248815c18ba2571081d2"
 
   def install
     bin.install "ambarictl"
