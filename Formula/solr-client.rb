ff1bcf4b010da425595fe733bdb0cfba4eb95c9d
oleewere <oleewere@gmail.com>
2019-01-09 02:01:23 +0100

    Brew formula update for go-solr-client version v0.2.0

--- Formula/solr-client.rb
+++ Formula/solr-client.rb
@@ -1,9 +1,9 @@
 class SolrClient < Formula
   desc "Software to manage multiple Ambari servers."
   homepage "https://github.com/oleewere"
-  url "https://github.com/oleewere/go-solr-client/releases/download/v0.1.0/go-solr-client_0.1.0_macOS_64-bit.tar.gz"
-  version "0.1.0"
-  sha256 "af1477e24e5c0ffe8d23b1d88ff0044384e06cdcd343c318af58746fd096aaa1"
+  url "https://github.com/oleewere/go-solr-client/releases/download/v0.2.0/go-solr-client_0.2.0_macOS_64-bit.tar.gz"
+  version "0.2.0"
+  sha256 "3fbc947ad3a5356555d44926156430b42f7919bd60d9c53ae2e7756617e7e6a0"
 
   def install
     bin.install "solr-client"
