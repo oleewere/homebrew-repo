13777e0cc4828d824d18b908b64b3f8f070a0129
oleewere <oleewere@gmail.com>
2019-05-05 13:28:40 +0200

    Brew formula update for cmctl version v0.1.0

--- Formula/cmctl.rb
+++ Formula/cmctl.rb
@@ -3,7 +3,7 @@ class Cmctl < Formula
   homepage "https://github.com/oleewere"
   url "https://github.com/oleewere/cmctl/releases/download/v0.1.0/cmctl_0.1.0_macOS_64-bit.tar.gz"
   version "0.1.0"
-  sha256 "1cfcc52c2ef0f29623c38c3d9181bcfad017039ab0a395fe3f43e002f9b2ecf8"
+  sha256 "4287758f61d23d0be4e72fffa01fe207981a27355d77322970edeea684d278b6"
 
   def install
     bin.install "cmctl"
