d98ccbc8e8ed47d7ab3c8a4d87ce5ee6b5176bb5
oleewere <oleewere@gmail.com>
2019-05-12 21:44:48 +0200

    Brew formula update for cmctl version v0.3.0

--- Formula/cmctl.rb
+++ Formula/cmctl.rb
@@ -1,9 +1,9 @@
 class Cmctl < Formula
   desc "Software to manage multiple Ambari servers."
   homepage "https://github.com/oleewere"
-  url "https://github.com/oleewere/cmctl/releases/download/v0.2.0/cmctl_0.2.0_macOS_64-bit.tar.gz"
-  version "0.2.0"
-  sha256 "19d0cc652f33f789f9c5619dc8b4d6370889b73572513675d2e2c1b4ffd1216f"
+  url "https://github.com/oleewere/cmctl/releases/download/v0.3.0/cmctl_0.3.0_macOS_64-bit.tar.gz"
+  version "0.3.0"
+  sha256 "5e270a5c55170f3c897e30b052a62730903a7052a38bbb41c65033e24f89684a"
 
   def install
     bin.install "cmctl"
